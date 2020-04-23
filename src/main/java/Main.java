2838e5a17b3acb4f4d5da8cae5ec6638aade1207
Caleb Carlson <cacaleb@rams.colostate.edu>
2020-04-23 16:39:03 -0600

    Fix path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         //DataReader.generateDumps();
-        CheckoutXmlParser parser = new CheckoutXmlParser("./data/Libdata.xml");
-        StatementExecutor.executeStatements(parser.checkouts);
+        if (args.length > 0) {
+            String libdata = args[0];
+            CheckoutXmlParser parser = new CheckoutXmlParser(libdata);
+            StatementExecutor.executeStatements(parser.checkouts);
+            System.out.println("Program executed successfully.");
+        }
+
+
 
     }
 }
