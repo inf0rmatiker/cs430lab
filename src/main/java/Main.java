de7cf869cd96f74467b9f71d63558cfecab70ff6
Caleb Carlson <cacaleb@rams.colostate.edu>
2020-04-23 16:35:14 -0600

    Uncomment statementExecutor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         //DataReader.generateDumps();
-        //StatementExecutor.executeStatements();
         CheckoutXmlParser parser = new CheckoutXmlParser("./data/Libdata.xml");
-        for (Checkout c: parser.checkouts) {
-            System.out.println(c);
-        }
+        StatementExecutor.executeStatements(parser.checkouts);
 
     }
 }
