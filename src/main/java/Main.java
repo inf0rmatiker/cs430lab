54f9e0be899689b03c433a72b32e6b9ea01420b3
Caleb Carlson <ccarlson355@gmail.com>
2020-04-25 20:58:50 -0600

    Add usage message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 public class Main {
 
+    private static void printUsageMessage() {
+        System.out.println("Description:\n\tRequest file must be an XML file containing only checkin or checkout requests.");
+        System.out.println("\tBefore running, system variables SQLUSERNAME and SQLPASSWORD must be set appropriately.\n");
+        System.out.println("Usage:\n\t$ java Main <path_to_request_file>");
+    }
+
     public static void main(String[] args) {
         //DataReader.generateDumps();
-        if (args.length > 0) {
+        if (args.length == 1) {
             String libdata = args[0];
             CheckoutXmlParser parser = new CheckoutXmlParser(libdata);
 
@@ -14,8 +20,10 @@ public class Main {
                 System.out.println("Program executed successfully.");
             }
         }
-
-
+        else {
+            printUsageMessage();
+            System.exit(1);
+        }
 
     }
 }
