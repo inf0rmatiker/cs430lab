238c5581bdea684688ba5084bd6001c1a7552e9e
Caleb Carlson <ccarlson355@gmail.com>
2020-04-25 21:29:14 -0600

    Create README

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# cs430lab3
+# CS430 Lab JDBC Application
+
+## Description
+This package has two goals:
+1. Parsing library csv records into .dump files, to be bulk-loaded into the database.
+2. Parsing XML files containing check-out and check-in requests, executing those requests, 
+and updating the database accordingly.
+
+## Usage
+**Before:** Compile the project: `$ mvn clean && mvn package`
+
+To execute the first goal as stated in the description:
+1. `$ cd target/classes/`, and execute **Main**:
+   - `$ java Main --dumps`
+
+
+To execute the second goal as stated in the description:
+1. Reset the database back to the state of Lab 3:
+   - Log into the **faure** database, and `$ use <your_database_name>;`
+   - Execute **Restart.sql** by running `$ source Restart.sql;`
+
+2. Set the **SQLUSERNAME** and **SQLPASSWORD** environment variables:
+   - `$ export SQLUSERNAME=<your_faure_username>`
+   - `$ export SQLPASSWORD=<your_faure_password>`
+   - Optionally, you may skip this step and provide them at runtime.
+
+3. `$ cd target/classes/`, and execute **Main**:
+   - `$ java Main <path_to_request_file>`
+   - Or, if you skipped step 3:
+       - `$ SQLUSERNAME=<username> SQLPASSWORD=<password> java Main <path_to_request_file>`
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,27 @@ public class Main {
         System.out.println("Description:\n\tRequest file must be an XML file containing only checkin or checkout requests.");
         System.out.println("\tBefore running, system variables SQLUSERNAME and SQLPASSWORD must be set appropriately.\n");
         System.out.println("Usage:\n\t$ java Main <path_to_request_file>");
+        System.out.println("\tOR");
+        System.out.println("\t$ java Main (-d, --dumps)");
     }
 
     public static void main(String[] args) {
         //DataReader.generateDumps();
         if (args.length == 1) {
-            String libdata = args[0];
-            CheckoutXmlParser parser = new CheckoutXmlParser(libdata);
-
-            if (parser.checkouts.isEmpty()) {
-                System.out.println("No checkin/out records to process");
+            if (args[0].equals("-d") || args[0].equals("--dumps")) {
+                DataReader.generateDumps();
             }
             else {
-                StatementExecutor.executeStatements(parser.checkouts);
-                System.out.println("Program executed successfully.");
+                String libdata = args[0];
+                CheckoutXmlParser parser = new CheckoutXmlParser(libdata);
+
+                if (parser.checkouts.isEmpty()) {
+                    System.out.println("No checkin/out records to process");
+                }
+                else {
+                    StatementExecutor.executeStatements(parser.checkouts);
+                    System.out.println("Program executed successfully.");
+                }
             }
         }
         else {
